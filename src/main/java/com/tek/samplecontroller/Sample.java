5dce1fb54eb8b76873fd05f7dde5fb38e16eca23
Nenavath, Bramaram <bnenavat@HSC-PF0U3LSN.allegisgroup.com>
2020-04-04 22:58:00 +0530

    "Added logging"

--- src/main/java/com/tek/samplecontroller/Sample.java
+++ src/main/java/com/tek/samplecontroller/Sample.java
@@ -1,5 +1,7 @@
 package com.tek.samplecontroller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api")
 public class Sample {
 	
+	private static final Logger log = LoggerFactory.getLogger(Sample.class);
+	
 	@RequestMapping("/health")
 	public String healthCheck() {
+		log.info(" SAMPLE CLASS :: Checking service health");
 		return "OK";
 	}
 	
